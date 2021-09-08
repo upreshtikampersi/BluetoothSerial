dbca9a5a443c0abd279e0275d91e8730b0a94f2c
Akshay Puri <54668622+akshaypuri1989@users.noreply.github.com>
2021-09-08 16:09:38 +0530

    Update BluetoothSerial.java

--- src/android/com/megster/cordova/BluetoothSerial.java
+++ src/android/com/megster/cordova/BluetoothSerial.java
@@ -331,15 +331,15 @@ public class BluetoothSerial extends CordovaPlugin {
         };
 
         Activity activity = cordova.getActivity();
-        activity.registerReceiver(discoverReceiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
-        activity.registerReceiver(discoverReceiver, new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED));
-        activity.registerReceiver(discoverReceiver, new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_STARTED));
+        this.registerReceiver(discoverReceiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
+        this.registerReceiver(discoverReceiver, new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED));
+        this.registerReceiver(discoverReceiver, new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_STARTED));
         if (bluetoothAdapter.isDiscovering()){
             LOG.d(TAG, "###discoverUnpairedDevices isDiscovering true###");
             bluetoothAdapter.cancelDiscovery();
         }
         bluetoothAdapter.startDiscovery();
-        LOG.d(TAG, "###discoverUnpairedDevices discovery started###");
+        LOG.d(TAG, "###discoverUnpairedDevices discovery started 111111###");
     }
 
     private JSONObject deviceToJSON(BluetoothDevice device) throws JSONException {
